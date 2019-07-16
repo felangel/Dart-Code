09eead3e4346c6b5a966872d4394ebaad08dfc27
Danny Tuppeny <danny@tuppeny.com>
2019-07-16 16:15:33 +0100

    More strict mode fixes

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -141,7 +141,7 @@ export async function attachLoggingWhenExtensionAvailable(attempt = 1) {
 }
 
 function setupTestLogging(): boolean {
-	const ext = vs.extensions.getExtension(dartCodeExtensionIdentifier);
+	const ext = vs.extensions.getExtension(dartCodeExtensionIdentifier)!;
 	if (!ext.isActive || !ext.exports)
 		return false;
 
@@ -591,7 +591,7 @@ export function ensureCompletion(items: vs.CompletionItem[], kind: vs.Completion
 }
 
 export async function resolveCompletion(completion: vs.CompletionItem): Promise<vs.CompletionItem> {
-	const resolved = await extApi.completionItemProvider.resolveCompletionItem(completion as DelayedCompletionItem, fakeCancellationToken);
+	const resolved = await extApi.completionItemProvider.resolveCompletionItem!(completion as DelayedCompletionItem, fakeCancellationToken);
 	return resolved || completion;
 }
 
